09dbd249fb185b3f7e11caa4ab3467a2755ce9f1
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-10 22:59:52 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,40 @@
 def division(num1, num2)
+  total = num1/num2
  
 end
+division(42, 7)
+
 
 def assign_variable(value)
+  new_value = value
   
 end
+assign_variable("Bob")
 
-def argue
+def argue(phrase)
+ puts phrase 
   
 end
+argue("I'm right and you're wrong!")
 
-def greeting
+def greeting(phrase, name)
+  puts "#{phrase}. My name is #{name}"
+
+end
+greeting("Hi there", "Bobby")
+
+def return_a_value(phrase = "Nice")
+  phrase
 
 end
 
-def return_a_value
+def last_evaluated_value(phrase = "expert")
+  return phrase
 
 end
 
-def last_evaluated_value
+def pizza_party(option1 ="cheese", x)
+  puts " I want to order #{option1} and #{x}}."
 
 end
-
-def pizza_party
-
-end
+pizza_party(option1,"pepperoni")
